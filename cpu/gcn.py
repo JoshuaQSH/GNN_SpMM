e1426bc627d6ccfd2796910f288eb2b655736d42
shenghao <1249646094@qq.com>
2022-04-11 13:07:00 +0000

    model added

---
+++ cpu/gcn.py
@@ -0,0 +1,175 @@
+import numpy as np
+import scipy.sparse as sp
+
+def softmax(X):
+    exp_up = np.exp(X)
+    exp_down = np.sum(exp_up,axis=1)[:,np.newaxis] # np.newaxis can add a new axis into the original matrix
+    return exp_up/exp_down
+
+def softmax_cross_entropy_deriv(X,Y):
+    return X - Y
+
+def relu(X):
+    return np.maximum(X, np.zeros(X.shape))
+
+def relu_diff(X):
+    return (X>0).astype(int)
+
+class numpyGCN(object):
+    """docstring for numpyGCN"""
+    def __init__(self, input_dim, hidden_dim, output_dim, dropout=None, weight_decay=0):
+        super(numpyGCN, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+        self.in_1 = None
+        self.out_1 = None
+        self.in_2 = None
+        self.out_2 = None
+        self.random_noise = False
+
+        self.dropout = dropout
+        self.weight_decay = weight_decay
+
+        # randomly initialize weight matrices according to Glorot & Bengio (2010)
+        self.W_1 = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (input_dim, hidden_dim))
+        self.W_2 = np.random.uniform(-np.sqrt(1./output_dim), np.sqrt(1./output_dim), (hidden_dim, output_dim))
+
+    def forward(self, X, A, drop_weights=None):
+        W_1 = self.W_1
+        W_2 = self.W_2
+        
+        
+        if drop_weights:
+            d1, d2 = drop_weights
+            W_1 = W_1 * d1
+            #W_2 = W_2 * d2
+            
+        
+        self.in_1 = A.dot(X).dot(W_1)
+        self.out_1 = relu(self.in_1)
+        
+        # Change the storing format here
+#         self.out_1 = sp.coo_matrix(self.out_1)
+#         self.out_1 = sp.csr_matrix(self.out_1)
+        self.out_1 = sp.csc_matrix(self.out_1)
+#         self.out_1 = sp.dia_matrix(self.out_1)
+#         self.out_1 = sp.bsr_matrix(self.out_1)
+#         self.out_1 = sp.lil_matrix(self.out_1)
+#         self.out_1 = sp.dok_matrix(self.out_1)
+        # self.out_1 = change_format(self.out_1)
+        
+        self.in_2 = A.dot(self.out_1).dot(W_2)
+        self.out_2 = softmax(self.in_2)
+        
+        return self.out_2
+
+
+    # argmax to predict the label
+    def predict(self, X, A):
+        out = self.forward(X, A)
+        return np.argmax(out, axis=1)
+
+
+    # returns the accuracy of the classifier
+    def compute_accuracy(self, X, Y, A, mask):
+        out = self.forward(X, A)
+        out_class = np.argmax(out[mask], axis=1)
+        expected_class = np.argmax(Y[mask], axis=1)
+        num_correct = np.sum(out_class == expected_class).astype(float)
+
+        return num_correct / expected_class.shape[0]
+
+    # normalized cross entropy loss
+    def calc_loss(self, X, Y, A, mask):
+        N = mask.sum()
+        preds = self.forward(X, A)
+        loss = np.sum(Y[mask] * np.log(preds[mask]))
+        loss = np.asscalar(-loss) / N
+
+        if self.weight_decay:
+            l2_reg = np.sum(np.square(self.W_1)) * self.weight_decay / 2
+            loss = loss + l2_reg
+
+        return loss
+
+    def loss_accuracy(self, X, Y, A, mask):
+        """ Combination of calc_loss and compute_accuracy to reduce the need to forward propagate twice """
+
+        # from calc_loss
+        N = mask.sum()
+        preds = self.forward(X, A)
+        loss = np.sum(Y[mask] * np.log(preds[mask]))
+        loss = np.asscalar(-loss) / N
+
+        if self.weight_decay:
+            l2_reg = np.sum(np.square(self.W_1)) * self.weight_decay / 2
+            loss = loss + l2_reg
+
+        # from compute_accuracy
+        out = preds
+        out_class = np.argmax(out[mask], axis=1)
+        expected_class = np.argmax(Y[mask], axis=1)
+        num_correct = np.sum(out_class == expected_class).astype(float)
+        accuracy = num_correct / expected_class.shape[0]
+
+        return loss, accuracy
+
+    # back propagation
+    def backprop(self, X, Y, A, mask):
+        dW_1 = np.zeros(self.W_1.shape)
+        dW_2 = np.zeros(self.W_2.shape)
+
+        if self.random_noise:
+            tmp_W1, tmp_W2 = np.copy(self.W_1), np.copy(self.W_2)
+            self.W_1 += np.random.normal(0, 0.001, self.W_1.shape)
+            self.W_2 += np.random.normal(0, 0.001, self.W_2.shape)
+
+        # divide by d so expectation of GCN layer doesn't change from train to test
+        if self.dropout:
+            d1 = np.random.binomial(1, (1-self.dropout), size=self.W_1.shape) / (1-self.dropout)
+            d2 = np.random.binomial(1, (1-self.dropout), size=self.W_2.shape) / (1-self.dropout)
+            preds = self.forward(X, A, (d1,d2))
+        else:
+            preds = self.forward(X, A)
+        
+        
+        # IMPORTANT: update gradient based only on masked labels
+        preds[~mask] = Y[~mask]
+
+        # last layer bp for cross entropy loss with softmax activation
+
+        dL_dIn2 = softmax_cross_entropy_deriv(preds, Y)
+        dIn2_dW2 = A.dot(self.out_1).transpose()
+        # change_format(dL_dIn2)
+        dL_dW2 = dIn2_dW2.dot(dL_dIn2)
+
+        # apply backprop for next layer
+        dL_dOut1 = A.transpose().dot(dL_dIn2).dot(self.W_2.transpose())
+        dOut1_dIn1 = relu_diff(self.in_1)
+        dL_dIn1 = dL_dOut1 * dOut1_dIn1
+        dIn1_dW1 = A.dot(X).transpose()
+
+        
+        dL_dW1 = dIn1_dW1.dot(dL_dIn1)
+
+        if self.weight_decay:
+            dL_dW1 += self.weight_decay * self.W_1
+
+        if self.dropout:
+            dL_dW1 *= d1
+            #dL_dW2 *= d2
+
+        if self.random_noise:
+            self.W_1, self.W_2 = tmp_W1, tmp_W2
+
+        return (dL_dW1, dL_dW2)
+
+
+    def gd_update(self, X, Y, A, mask, lr):
+        # compute weight gradients
+        dW_1, dW_2 = self.backprop(X, Y, A, mask)
+
+        # parameter update
+        self.W_1 -= dW_1 * lr
+        self.W_2 -= dW_2 * lr
--- cpu/utils.py
+++ cpu/utils.py
@@ -1,5 +1,7 @@
 import json
 import numpy as np
+import scipy as sp
+import time
 
 def read_raw_data(root_data='../dataset/s_mm.txt'):
     with open(root_data, 'r') as f:
@@ -50,3 +52,190 @@ def label_Y(time_in, mem_in):
         label_new.append(final_dict[0][0])
     
     return np.array(label_new)
+
+def change_format(in_mat):
+    start = time.time()
+    index2name = {1:'COO', 2:'CSR', 3:'CSC', 4:'BSR', 5:'DIA', 6:'LIL'}
+    attributeList = calAttributes(in_mat)
+    attributeList_ = np.array(attributeList).reshape(1,19)
+    index = int(model_xg.predict(attributeList_)[0])
+    end = time.time()
+    print("Inference Time: {:.4f}s".format(end - start))
+    print("Format: ",index2name[index])
+
+    if index == 1:
+        return sp.coo_matrix(in_mat)
+    elif index == 2:
+        return sp.csr_matrix(in_mat)
+    elif index == 3:
+        return sp.csc_matrix(in_mat)
+    elif index == 4:
+        return sp.bsr_matrix(in_mat)
+    elif index == 5:
+        return sp.dia_matrix(in_mat)
+    elif index == 6:
+        return sp.lil_matrix(in_mat)
+
+def normalize_features(mx):
+    """Row-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0. # whether the element is plus or minus infinity
+    r_mat_inv = sp.diags(r_inv)
+    mx = sp.coo_matrix(mx)
+    mx = r_mat_inv.dot(mx)
+    return mx
+
+# coordinate list sparse matrix for normalized adjacency matrix
+def normalize_adj(adj):
+    """Symmetrically normalize adjacency matrix."""
+    adj = sp.csr_matrix(adj)
+    adj += sp.eye(adj.shape[0])
+    rowsum = np.array(adj.sum(axis=1))
+    d_inv_sqrt = np.power(rowsum, -0.5).flatten()
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
+    return adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).tocsr()
+
+# Function to calculate all attributes of a sparse matrix required to train model
+def calAttributes(matData, memOrtime='time'):
+    if(sp.isspmatrix_coo(matData) == False):
+        matData = sp.coo_matrix(matData)
+    
+    # variable list to store all attributes of sparse matrix required to train model 
+    attributeList = []
+    
+    # Compute the Cost Time of extracting the features
+    startTime = time.time()
+    
+    # writing number of rows and columns in list
+    numRows = matData.shape[0]
+    numCol = matData.shape[1]
+    attributeList.append(numRows)
+    attributeList.append(numCol)
+
+    # writing number of non-zeros in list
+    nnz = matData.count_nonzero()
+    attributeList.append(nnz)
+
+    # writing number of diagonals in list
+    # Ndiags = numCol + numRows - 1
+    # attributeList.append(Ndiags)
+
+    # attributes for nnzs per row
+    rowArr = matData.row
+    nnzRows = np.full(matData.shape[0], 0, float)
+
+    for i in range(rowArr.size):
+        nnzRows[rowArr[i]] += 1
+
+    aver_RD = np.mean(nnzRows)
+    max_RD = np.max(nnzRows)
+    min_RD = np.min(nnzRows)
+    dev_RD = np.std(nnzRows)
+
+    attributeList.append(aver_RD)
+    attributeList.append(max_RD)
+    attributeList.append(min_RD)
+    attributeList.append(dev_RD)
+
+    #attributes for nnzs per col
+    colArr = matData.col
+    nnzCol = np.full(matData.shape[1], 0, float)
+
+    for i in range(colArr.size):
+        nnzCol[colArr[i]] += 1
+
+    aver_CD = np.mean(nnzCol)
+    max_CD = np.max(nnzCol)
+    min_CD = np.min(nnzCol)
+    dev_CD = np.std(nnzCol)
+
+    attributeList.append(aver_CD)
+    attributeList.append(max_CD)
+    attributeList.append(min_CD)
+    attributeList.append(dev_CD)
+
+    # calculating ER_DIA (Optional)
+    matDia = matData.todia()
+    matDiaData = matDia.data
+    ER_DIA = (np.count_nonzero(matDiaData))/(matDiaData.shape[0]*matDiaData.shape[1])
+    attributeList.append(ER_DIA)
+
+    # calculating ER_RD
+    ER_RD = nnz/(max_RD*numRows)
+    attributeList.append(ER_RD)
+
+    # calculating ER_CD
+    ER_CD = nnz/(numCol*max_CD)
+    attributeList.append(ER_CD)
+
+    # calculating row_bounce and col_bounce
+    diffAdjNnzRows = np.full(nnzRows.size - 1, 0, float)
+    for i in range(1,nnzRows.size):
+        diffAdjNnzRows[i-1] = np.absolute(nnzRows[i] - nnzRows[i-1])
+
+    row_bounce = np.mean(diffAdjNnzRows)
+
+    diffAdjNnzCols = np.full(nnzCol.size - 1, 0, float)
+    for i in range(1,nnzCol.size):
+        diffAdjNnzCols[i-1] = np.absolute(nnzCol[i] - nnzCol[i-1])
+
+    col_bounce = np.mean(diffAdjNnzCols)
+
+    attributeList.append(row_bounce)
+    attributeList.append(col_bounce)
+
+    # calculating density of matrix
+    densityOfMatrix = (matData.count_nonzero())/((matData.shape[0])*(matData.shape[1]))
+    attributeList.append(densityOfMatrix)
+
+    # calculating normalized variation of nnz per row
+    nnzRowsNormalised = (nnzRows-min_RD)/max_RD
+    cv = np.var(nnzRowsNormalised)
+    attributeList.append(cv)
+
+    # caluculating max_mu
+    max_mu = max_RD - aver_RD
+    attributeList.append(max_mu)
+    
+    # Compute the Cost Time of extracting the features
+    endTime = time.time()
+    print("Matrix Shape: {}, Extration Time: {}".format(matData.shape[0], round((endTime-startTime),4)))
+
+    return attributeList
+
+def train_with_gd(model, features, adj, y_train, y_val, train_mask, val_mask, early_stopping, lr, epochs):
+    t_total = time.time()
+    best_val_loss, val_epoch = float('inf'), 0
+    past_loss = float('inf')
+    elapsed = []
+    for epoch in range(epochs):
+        start = time.time()
+        model.gd_update(features, y_train, adj, train_mask, lr)
+        end = time.time()
+
+        train_loss, train_accuracy = model.loss_accuracy(features, y_train, adj, train_mask)
+        val_loss, val_accuracy = model.loss_accuracy(features, y_val, adj, val_mask)
+
+        elapsed.append(end - start)
+        print("Epoch:", '%04d' % (epoch + 1), "train_loss=", "{:.5f}".format(train_loss),
+          "train_acc=", "{:.5f}".format(train_accuracy), "val_loss=", "{:.5f}".format(val_loss),
+          "val_acc=", "{:.5f}".format(val_accuracy), "time=", "{:.5f}".format(elapsed[epoch]))
+
+        # decrease the learning rate if the train loss increased
+        if train_loss > past_loss:
+            lr *= 0.5
+        train_loss = min(train_loss, past_loss)
+
+        if early_stopping:
+            if val_loss < best_val_loss:
+                best_val_loss = val_loss
+                val_epoch = epoch
+            else:
+                if epoch - val_epoch == 10:
+                    print("validation loss has not improved for 10 epochs... stopping early")
+                    break
+
+    print("Total time: {:.4f}s".format(time.time() - t_total))
+    return elapsed
